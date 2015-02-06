ea346e4955a540c71f766b8b0256fbb823fb3f6d
shigemk2 <i.am.shige@gmail.com>
2015-02-06 21:30:47 +0900

    Example: notexist

--- chapter02/notexist.ts
+++ chapter02/notexist.ts
@@ -2,3 +2,13 @@ var d: any;
 d = new Date();
 // notexistなるメンバーはDate型に存在しない
 console.log(d.notexist);
+
+var date: Date;
+date = new Date();
+console.log(date.notexist); // コンパイルエラー
+
+var obj = {};
+console.log(obj.notExist); // コンパイルエラー
+
+var a:number;
+console.log(a); // undefined
