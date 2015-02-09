c72f360b8789398a6d5464defb66806eb4ab5111
shigemk2 <i.am.shige@gmail.com>
2015-02-09 20:41:19 +0900

    Example: array

---
+++ chapter02/array.ts
@@ -0,0 +1,9 @@
+var array: any[] = ["Hello!", "World!"];
+for (var i: number = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+var arrays: string[] = ["Hello!", "World!"];
+for (var i: number = 0; i < arrays.length; i++) {
+    console.log(arrays[i]);
+}
