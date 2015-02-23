29806ee81ed528f1b10c9dc953dd80b002f4ba40
shigemk2 <i.am.shige@gmail.com>
2015-02-23 14:29:36 +0900

    初期化のあとなら利用しても構わないけど、今初期化しようとしているクラスのメンバー自身は参照しないほうが良いだろう

--- chapter02/annotation.ts
+++ chapter02/annotation.ts
@@ -2,6 +2,8 @@ class A {
     static a: number = A.b + A.c;
     static b: number = 1;
     static c: number = 2;
+    static d: number = A.b + A.c; // 3
 }
 console.log(A.a); // NaN
 console.log(A.a.toString()); // NaN
+console.log(A.d); // 3
