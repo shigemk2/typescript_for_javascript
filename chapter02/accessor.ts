70040b5f31c08b5b9d70da2f64df3b40b60af454
shigemk2 <i.am.shige@gmail.com>
2015-02-27 21:50:35 +0900

    Fix accessor

--- chapter02/accessor.ts
+++ chapter02/accessor.ts
@@ -1,5 +1,7 @@
 class A {
     private message: string;
+    // accessor.ts(3,9): error TS1056: Accessors are only available when targeting ECMAScript 5 and higher.
+    // tsc -t ES5でやらないとコンパイルエラーになる
     set sample(str: string) { this.message = str; }
     get sample(): string { return this.message; }
 }
